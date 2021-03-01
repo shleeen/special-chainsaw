24d981172c1ec80f551966ae844998e9fd526932
Sharlene <sharlenedsilva@gmail.com>
2021-03-01 12:57:14 +0000

    plotterrere

--- plotterer.py
+++ plotterer.py
@@ -1,25 +1,54 @@
 import matplotlib.pyplot as plt
 import csv
 
-x=[] #time
-y=[]
+def meanFitvsGenerations():
+    gen=[]
+    mean_fit=[]
+    
+    with open('MeanFitness.csv', 'r') as csvfile:
+        csvfile = open('MeanFitness.csv', 'r') 
+        plots= csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            gen.append(float(row[0]))
+            mean_fit.append(float(row[1]))
+
+    f1 = plt.figure()
+    plt.plot(gen, mean_fit, marker='o')
+
+    plt.title('Mean Fitness vs Generations')
+    plt.xlabel('Generations')
+    plt.ylabel('Mean Fitness')
+    # plt.show()
+    plt.savefig('MeanFitVsGen.png')
 
 
-with open('data0.csv', 'r') as csvfile:
-    plots= csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        x.append(float(row[1]))
-        y.append(float(row[0]))
+def bestFitvsGenerations():
+    gen=[]
+    best_fit=[]
+    
+    with open('FitnessVsGeneration.csv', 'r') as csvfile:
+        csvfile = open('FitnessVsGeneration.csv', 'r') 
+        plots= csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            gen.append(float(row[0]))
+            best_fit.append(float(row[1]))
+
+    plt.figure(0)
+    plt.plot(gen, best_fit) #, marker='x'
+
+    plt.title('Best Fitness vs Generations')
+    plt.xlabel('Generations')
+    plt.ylabel('Best Fitness')
+    plt.savefig('BestFitVsGen.png')
 
 
-plt.plot(x,y, marker='o')
+def main():
+    # call the plotter functions here
+    bestFitvsGenerations()
+    meanFitvsGenerations()
 
-plt.title('Data from the CSV File ')
+    print("something has been plotted. :) ")
 
-plt.xlabel('time')
-plt.ylabel('target position')
-plt.show()
-plt.savefig('A_PLOT.png')
 
-print("hello??")
-
+if __name__ == "__main__":
+    main()
